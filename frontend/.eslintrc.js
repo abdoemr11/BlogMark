5b55ffafe5ce190ca0e8ab69fa6a9acfd21203be
abdoemr11 <abdoemr11@gmail.com>
2023-05-06 22:14:20 +0300

    fix line break error it should work now on windows or linux

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -27,7 +27,7 @@ module.exports = {
     ],
     'linebreak-style': [
       'error',
-      'windows'
+      process.platform === 'win32' ? 'windows' : 'unix'
     ],
     'quotes': [
       'error',
